9bccac2f8f89da3c8193638c1b90e07fa1680f54
Simon <32335308+fattal28@users.noreply.github.com>
2018-05-06 17:36:14 +0100

    add string copy
    This accepts a string as input and returns a pointer to an array holding a copy of that string.

---
+++ stringcopy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *copy(char *array);
+
+int main(void){
+	char array[100];
+	char *arraycopy;
+	printf("Enter your string: \n");
+	scanf("%s", array);
+	printf("Pointer to array holding a copy of the string is: %p\n", &arraycopy);
+}
+
+char *copy(char *array){
+	int i, counter = 0;
+	for(i = 0; i < strlen(array); i++){
+		counter++;
+	}
+	char *arraycopy = (char*) malloc (sizeof(char)*counter);
+	int j;
+	for(j = 0; j < strlen(arraycopy); j++){
+		arraycopy[counter] = array[counter];
+		counter++;
+	}
+	return arraycopy;
+}
+
