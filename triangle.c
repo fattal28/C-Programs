07bbcb57de55ce4a5bf837ba6529dc36df5b34c2
Simon <32335308+fattal28@users.noreply.github.com>
2018-05-06 17:29:19 +0100

    add files

---
+++ triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void triangle(int width, char x);
+
+int main(void){
+    triangle(5, 'E');
+}
+
+void triangle(int width, char x){
+    if (width < 2){
+        return;
+    }
+    int counter = 1;
+    int direction = 1;
+    do{
+        int i;
+        for(i = 0; i < width - counter; i++){
+            printf(" ");
+        }
+        for (int i = 0; i < counter; i++){
+            printf("%c", x);
+        }
+        printf("\n");
+        counter += direction;
+        if(counter > width){
+            counter = width - 1;
+            direction = -1;
+        }
+    }while (counter != 0);
+    return;
+}
